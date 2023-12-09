95d84390d081e16ca03ace82f18608cbc6c301a8
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-12-09 13:10:31 +0100

    Update import_truth.py

--- .github/scripts/import_truth.py
+++ .github/scripts/import_truth.py
@@ -25,7 +25,8 @@ url = url.format(snapshot_date=snapshot_date)
 try:
     response = urlopen(url)
 except urllib.error.HTTPError:
-    sys.exit(0)
+    print (f'Http error - failed to access url {url}')
+    sys.exit(1)
 
 
 # Read data
